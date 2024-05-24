85bb1ace5a3db141237640ab92fa543eb53cf378
Suraj kumar <kumarsrj2023@gmail.com>
2024-05-25 04:34:16 +0530

    create studetn model and controller and migrattion file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\CheckUserRole;
+use App\Livewire\Admin\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,8 +14,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard')->middleware(['checkRole:admin']);
 
-
+// For user login
 Route::get('login', [UserController::class, 'loginPage'])->name('blade.login');
 Route::get('register', [UserController::class, 'registerPage'])->name('blade.register');
 Route::post('savelogin', [UserController::class, 'login'])->name('user.login');
 Route::post('saveregister', [UserController::class, 'register'])->name('user.register');
+
+Route::middleware('checkRole:admin')->group(function(){
+    Route::resource('admin/student', StudentController::class);
+});
